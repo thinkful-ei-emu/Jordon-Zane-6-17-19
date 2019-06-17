057893e5ae2149804b2b006bc32590141f815003
Zane Kuehn <zanekuehn@yahoo.com>
2019-06-17 13:14:08 -0500

    moved files to src

--- NPM-Packages-and-Webpack/src/index.js
+++ NPM-Packages-and-Webpack/src/index.js
@@ -1,13 +1,20 @@
 import $ from 'jquery';
+import 'normalize.css';
+import api from './api';
+import store from './store';
+import shoppingList from './shopping-list';
+import './index.css';
+/* global shoppingList, store, api */
 
-function main() {
-  console.log('DOM is loaded');
+$(document).ready(function() {
+  shoppingList.bindEventListeners();
 
-  const startMsg = $('<p>Webpack is working!</p>');
-  $('#root').append(startMsg);
-  
-}
-console.log('whatever i want')
-console.log('stuff')
+  // On initial load, fetch Shopping Items and render
+  api.getItems()
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    })
+    .catch(err => console.log(err.message));
+});
 
-$(main);
